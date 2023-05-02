e81e4ad125835a6b1fbe4e63593e889fa5a8e17f
Tauqeer <hellotauqeer@gmail.com>
2023-05-02 23:04:13 +0530

    added basic of frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Login from './components/Login/Login'
+import Register from './components/Register/Register'
+import Homepage from './components/Homepage/Homepage'
+
+
+export default function App() {
+  return (
+    <>
+    <Homepage></Homepage>
+    <Register></Register>
+    <Login></Login>
+    
+    </>
+  )
+}
---
+++ frontend/src/components/Homepage/Homepage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Homepage.css"
+
+// const mystyle = {
+//     color : "orange", 
+//     fontSize : "40px",
+//     backgroundColor : 'blue',
+//     width : '10%'
+// }
+export default function Homepage() {
+  return (
+    <div className='homepage'>
+        <h1>Hello Homepage</h1>
+        <div className='button'>Logout</div>      
+    </div>
+  )
+}
