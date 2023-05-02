65bdcf1309a19571e18eb1b4205e6e2531b3667e
Tauqeer <hellotauqeer@gmail.com>
2023-05-03 00:12:21 +0530

    added App.css config for home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,16 @@ import React from 'react'
 import Login from './components/Login/Login'
 import Register from './components/Register/Register'
 import Homepage from './components/Homepage/Homepage'
-
+import './App.css'
 
 export default function App() {
   return (
     <>
+    <div className="App">
     <Homepage></Homepage>
     <Register></Register>
     <Login></Login>
+    </div>
     
     </>
   )
