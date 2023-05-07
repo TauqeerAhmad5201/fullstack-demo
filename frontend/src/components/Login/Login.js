09e46e17e6863db88bf12e1116b2291bfeafc799
Tauqeer <hellotauqeer@gmail.com>
2023-05-07 20:42:47 +0530

    removed extra button

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -75,15 +75,6 @@ export default function Login() {
                 />
               </div>
             </div>
-
-            <div>
-              <button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-              Login
-              </button>
-            </div>
             <div className='center-div'>or</div>
             <div>
               <button
