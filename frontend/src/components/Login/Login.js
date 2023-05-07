d7717912a21ee1ee46154055367747decb4f73d2
Tauqeer <hellotauqeer@gmail.com>
2023-05-07 13:36:02 +0530

    raw logic for login in Logic.js

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
+import axios from 'axios'
 
 export default function Login() {
   const [user, setUser] = useState({
@@ -13,6 +14,12 @@ export default function Login() {
     setUser({
       ...user, [name]:value           // now changing the input field name to the value fetch
     })}
+
+    const login = (e) => {
+      e.preventDefault();
+      axios.post("http://localhost:9002/login", user)
+      .then(res => console.log(res))
+    }
   return (
     <>
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
@@ -82,8 +89,9 @@ export default function Login() {
               <button
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                onClick={login}
               >
-                Register
+                Login 
               </button>
             </div>
           </form>
