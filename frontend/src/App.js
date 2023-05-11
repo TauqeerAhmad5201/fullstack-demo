562c6d478e0659694db69acc319189a13215e41c
Tauqeer <hellotauqeer@gmail.com>
2023-05-11 23:46:06 +0530

    added route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,24 @@
-import React from 'react'
-import Login from './components/Login/Login'
-import Register from './components/Register/Register'
-import Homepage from './components/Homepage/Homepage'
-import './App.css'
+import React from "react";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import Homepage from "./components/Homepage/Homepage";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+// import { Router } from "express";
 
 export default function App() {
   return (
     <>
-    <div className="App">
-    {/* <Homepage></Homepage> */}
-    {/* <Register></Register> */}
-    <Login></Login>
-    </div>
-    
+      <div className="App">
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<Homepage />} />
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/register" element={<Register />} />
+          </Routes>
+        </Router>
+      </div>
     </>
-  )
+  );
 }
