3671a003ac67c7af3037a702adfc5a3b1d08646c
Tauqeer <hellotauqeer@gmail.com>
2023-05-13 08:48:04 +0530

    adding logic for  checking login of an user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,24 @@ import Register from "./components/Register/Register";
 import Homepage from "./components/Homepage/Homepage";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import "./App.css";
 // import { Router } from "express";
 
 export default function App() {
+  const [user, setLoginUser] = useState({})
   return (
     <>
       <div className="App">
         <Router>
           <Routes>
-            <Route exact path="/" element={<Homepage />} />
-            <Route exact path="/login" element={<Login />} />
+            < Route exact path="/" element={
+                user && user._id ?  <Homepage /> : <Login setLoginUser={setLoginUser} />
+              
+            } /> 
+              
+          
+            <Route exact path="/login" element={<Login setLoginUser={setLoginUser}/>} />
             <Route exact path="/register" element={<Register />} />
           </Routes>
         </Router>
